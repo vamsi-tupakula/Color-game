e973c5681c49e8c99463812806fee67c2b9d8e4d
Vamsi <vamsitupakula474@gmail.com>
2021-08-27 19:12:57 +0530

    full game designed

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 let clrValue = document.getElementById("clr-value");
+let elem;
 
 window.addEventListener("load", function(){
     let pColor = randomColor();
-    let elem = Math.floor(Math.random()*7);
+    let h_bg = document.querySelector(".heading");
+    h_bg.style.backgroundColor = randomColor();
     
-    
+    elem = Math.floor(Math.random()*6 + 1); // (max-min) + min
+    console.log(elem);
+
     clrValue.innerHTML = pColor;
     for(let i=1;i<=6;i++){
         let sel =document.getElementById(i);
@@ -24,4 +28,15 @@ function randomColor(){
     let z = Math.floor(Math.random()*256);
 
     return `RGB(${x},${y},${z})`;
+}
+
+function check(userChoice){
+    userChoiceId = userChoice.id;
+    if(userChoiceId==elem){
+        clrValue.innerHTML = "CORRECT";
+    }
+    else{
+        let wrng = document.getElementById(userChoiceId);
+        wrng.style.cssText = "opacity: 0;transition: .4s ease-in-out";
+    }
 }
